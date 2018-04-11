a55fc696017727726f2a641aeb353ce3dfed3cf4
Garrett Forsyth <gtforsyth91@gmail.com>
2018-04-11 12:32:01 -0400

    added logic to respond to user guesses

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,20 @@
 class PhotosController < ApplicationController
 
+
   def show
     @photo = Photo.find(params[:id])
+    js :photo_id => @photo.id
+  end
+
+  def guess
+    @photo = Photo.find(params[:id])
+    name = params[:guessName].strip;
+    guessCoord = params[:guessCoord];
+    location = @photo.locations.select{ |l| l.character.name.eql?(name) }.first
+
+    respond_to do |format|
+      format.json { render json: { response: location.isCorrectGuess(guessCoord) } }
+    end
+
   end
 end
