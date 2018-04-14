fc19c3f9c782b1e25382b5685e902de5d246b2ec
Garrett Forsyth <gtforsyth91@gmail.com>
2018-04-14 13:57:19 -0400

    deleted change migration, edited create migration

--- db/migrate/20180411165146_create_games.rb
+++ db/migrate/20180411165146_create_games.rb
@@ -1,7 +1,7 @@
 class CreateGames < ActiveRecord::Migration[5.1]
   def change
     create_table :games do |t|
-      t.time :score
+      t.numeric :score
       t.string :user
 
       t.timestamps
--- db/migrate/20180413161749_change_game_score_to_decimal.rb
+++
@@ -1,5 +0,0 @@
-class ChangeGameScoreToDecimal < ActiveRecord::Migration[5.1]
-  def change
-    change_column :games, :score, 'numeric USING CAST(score AS numeric)'
-  end
-end
