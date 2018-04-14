c8a6d05e01d3672d7321630698bc48aa55d3e592
Garrett Forsyth <gtforsyth91@gmail.com>
2018-04-14 13:51:45 -0400

    fixed typo

--- db/migrate/20180413161749_change_game_score_to_decimal.rb
+++ db/migrate/20180413161749_change_game_score_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangeGameScoreToDecimal < ActiveRecord::Migration[5.1]
   def change
-    change_column :games, :score, 'numeric USING CAST(score AS numberic)'
+    change_column :games, :score, 'numeric USING CAST(score AS numeric)'
   end
 end
