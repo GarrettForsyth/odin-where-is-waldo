a49a5534e75f23e815eb6ee251ca1be80e2cd930
Garrett Forsyth <gtforsyth91@gmail.com>
2018-04-14 13:49:36 -0400

    changed cast in score migration

--- db/migrate/20180413161749_change_game_score_to_decimal.rb
+++ db/migrate/20180413161749_change_game_score_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangeGameScoreToDecimal < ActiveRecord::Migration[5.1]
   def change
-    change_column :games, :score, :decimal
+    change_column :games, :score, 'numeric USING CAST(score AS numberic)'
   end
 end
